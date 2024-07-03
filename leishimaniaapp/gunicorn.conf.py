9fc7bbd0385b84f45f28f2fae8aef8a47dd1e637
Lucas Bezerra <lbezerradev@gmail.com>
2024-07-03 15:06:19 -0300

    infra

--- leishimaniaapp/gunicorn.conf.py
+++ leishimaniaapp/gunicorn.conf.py
@@ -1,12 +1,3 @@
-from psycogreen.gevent import patch_psycopg
-
-
-def post_fork(_, worker):
-    patch_psycopg()
-    worker.log.info("Made Psycopg2 Green")
-
-
-worker_class = "gevent"
 workers = 1
 worker_connections = 1000
 
