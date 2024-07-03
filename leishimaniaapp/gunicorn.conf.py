2f63f8dd21a67b8fb109a2b020c80263c4f27e84
Lucas Bezerra <lbezerradev@gmail.com>
2024-07-03 19:12:38 -0300

    infra

--- leishimaniaapp/gunicorn.conf.py
+++ leishimaniaapp/gunicorn.conf.py
@@ -1,5 +1,16 @@
-workers = 1
-worker_connections = 1000
 
+# Configurações básicas
+workers = 1
+worker_class = "sync"
+worker_connections = 1000
 timeout = 45
 graceful_timeout = 45
+keepalive = 2
+
+# Caminho para os logs
+errorlog = "-"
+accesslog = "-"
+loglevel = "info"
+
+# Detalhes do logging
+capture_output = True
